4e4caa0bb68ef3ef7320d4bcd6136dc765605a63
kumar gaurav <kumar gaurav@DESKTOP-L339HC9>
2018-06-07 08:20:25 +0300

    initial commit

---
+++ SpringMvcJava/src/main/java/com/myclass1/web/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.myclass1.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.myclass")
+public class MvcConfig extends WebMvcConfigurerAdapter
+{
+
+}
